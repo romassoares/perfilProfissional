fc2389ed713f4971ada6168484984af4844d5d3a
Romario Soares <romassoares@hotmail.com>
2021-03-30 22:54:18 -0300

    nav welcome

---
+++ api.js
@@ -0,0 +1,16 @@
+async function usersGit() {
+    const user = await fetch('https://api.github.com/users/romassoares')
+    const result = await user.json()
+    const img = document.querySelector('#divImg')
+    if (img) {
+        const h = document.querySelector(".txtNav")
+        h.innerText = result.name
+        const t = document.querySelector("#fun")
+        t.innerText = result.bio
+        img.setAttribute('src', result.avatar_url)
+    }
+}
+function openMenu() {
+    console.log(e.target)
+
+}
